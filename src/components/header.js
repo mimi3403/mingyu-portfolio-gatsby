544c35deb3efb8a8ef78e054f2dee7af92ebf688
mimi3403 <mimicp9464@gmail.com>
2022-10-06 12:59:49 -0400

    adding styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { StyledHeader } from "../styles/styled-components";
+import { Link } from "gatsby";
 
 const Header = (props) => {
-  return <header>header</header>;
+  return (
+    <StyledHeader>
+      <h1>
+        <Link to="/">Mingyu Hong</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
 };
 
 export default Header;
---
+++ src/styles/styled-components.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: #333;
+  color: #fff;
+  height: 5rem;
+  padding: 0 1rem;
+  box-shadow: 1px 1px 3px 1px #808080;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  h1 {
+    margin: 0px;
+  }
+`;
