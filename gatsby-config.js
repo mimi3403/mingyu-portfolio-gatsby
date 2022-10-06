2eab93552386c45f2c19b25400f3d526607d17bf
mimi3403 <mimicp9464@gmail.com>
2022-10-06 11:59:37 -0400

    set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,26 @@
 module.exports = {
   siteMetadata: {
     title: `mingyu-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
+    author: "Mingyu Hong",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "pages"
-  }]
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,5 @@
 import React from "react";
+
+export default function about(props) {
+  return <h1>about</h1>;
+}
